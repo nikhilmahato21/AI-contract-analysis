548f4e6989b8af480b68325cebc2ae9128dd79f2
nikhilmahato21 <nikhilmahato3337@gmail.com>
2026-03-13 13:06:01 +0530

    feat: add react-query provider and api client

---
+++ client/src/hooks/use-current-user.ts
@@ -0,0 +1,23 @@
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useCurrentUser = () => {
+  const { data:user, isLoading, isError } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async () => {
+      try {
+        const response = await api.get("/auth/current-user");
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+        return null;
+      }
+    }
+  });
+
+  return {
+    user,
+    isLoading,
+    isError,
+  };
+};
